eaedd920635e0540dbe6d40a5723d9264c51dd10
Ihor Karpyn <igor82200@gmail.com>
2022-09-05 18:02:57 +0300

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,8 @@
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { HashRouter as Router } from 'react-router-dom';
-
-import { store } from './store';
 import { App } from './App';
 
-// Just a convenient component with all the wrappers for the `App`
-// The Router component (if you use it) should be placed inside the Provider
 const Root = () => (
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>
+  <App/>
 );
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<Root/>, document.getElementById('root'));
